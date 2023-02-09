a5635b86e6d96e4cbbdf8850dffb07fe24a5dbbb
Sabbirbro <sabbirhossainrifat014@gmail.com>
2023-02-09 22:33:07 +0600

    add-grading system problem

---
+++ problem-8.js
@@ -0,0 +1,19 @@
+// Setting up a grading system with Javascript 
+
+// Let’s assume the grades are out of 100
+
+// Create a variable to store the grade
+let grade = 90;
+
+// Use an if-else statement to assign grades
+if (grade >= 90) {
+    console.log("A");
+} else if (grade >= 80) {
+    console.log("B");
+} else if (grade >= 70) {
+    console.log("C");
+} else if (grade >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
